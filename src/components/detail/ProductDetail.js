8fa930ec455179c92c8011db30837127692ddd43
Quan0310 <114817413+Quan0310@users.noreply.github.com>
2024-05-22 14:24:36 +0700

    update 22/5 2.24pm

--- src/components/detail/ProductDetail.js
+++ src/components/detail/ProductDetail.js
@@ -29,48 +29,21 @@ const ProductDetail = (props) => {
     })
     const images = [
         {
-            original: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-            thumbnail: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
+            original: product.image,
+            thumbnail: product.image,
         },
         {
-            original: 'https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp',
-            thumbnail: 'https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp',
-        },
-        {
-            original: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-            thumbnail: 'https://t4.ftcdn.net/jpg/05/28/15/29/360_F_528152997_fElU6be6dBc3km1sj9MaWyiG9aE53w9n.jpg',
-        },
-        {
-            original: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-            thumbnail: 'https://t4.ftcdn.net/jpg/05/28/15/29/360_F_528152997_fElU6be6dBc3km1sj9MaWyiG9aE53w9n.jpg',
-        },
-        {
-            original: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-            thumbnail: 'https://t4.ftcdn.net/jpg/05/28/15/29/360_F_528152997_fElU6be6dBc3km1sj9MaWyiG9aE53w9n.jpg',
-        },
-        {
-            original: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-            thumbnail: 'https://t4.ftcdn.net/jpg/05/28/15/29/360_F_528152997_fElU6be6dBc3km1sj9MaWyiG9aE53w9n.jpg',
-        },
-        {
-            original: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-            thumbnail: 'https://t4.ftcdn.net/jpg/05/28/15/29/360_F_528152997_fElU6be6dBc3km1sj9MaWyiG9aE53w9n.jpg',
-        },
-        {
-            original: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-            thumbnail: 'https://t4.ftcdn.net/jpg/05/28/15/29/360_F_528152997_fElU6be6dBc3km1sj9MaWyiG9aE53w9n.jpg',
-        },
-        {
-            original: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-            thumbnail: 'https://t4.ftcdn.net/jpg/05/28/15/29/360_F_528152997_fElU6be6dBc3km1sj9MaWyiG9aE53w9n.jpg',
-        },
-        {
-            original: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-            thumbnail: 'https://t4.ftcdn.net/jpg/05/28/15/29/360_F_528152997_fElU6be6dBc3km1sj9MaWyiG9aE53w9n.jpg',
-        },
-        {
-            original: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-            thumbnail: 'https://t4.ftcdn.net/jpg/05/28/15/29/360_F_528152997_fElU6be6dBc3km1sj9MaWyiG9aE53w9n.jpg',
+            original: product.image,
+            thumbnail: product.image,
+        }, {
+            original: product.image,
+            thumbnail: product.image,
+        }, {
+            original: product.image,
+            thumbnail: product.image,
+        }, {
+            original: product.image,
+            thumbnail: product.image,
         },
     ];
     const reviews = [
